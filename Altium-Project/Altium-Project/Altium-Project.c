5d007e0402176d3df219d0af4b1073a3816bd8fb
Falkon353 <martin.mostad@ntebb.no>
2016-06-30 19:02:38 +0200

    Created the project Atmel Studio
    The start of the project where all the programing files shuld be

---
+++ Altium-Project/Altium-Project/Altium-Project.c
@@ -0,0 +1,17 @@
+/*
+ * Altium_Project.c
+ *
+ * Created: 30.06.2016 18:32:50
+ *  Author: Martin
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
